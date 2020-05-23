ccba072e01b65715d69d74580cb05dbeba9d161d
Steven Bister <stevebister@gmail.com>
2020-05-23 14:29:37 +0100

    Installed and connected the gatsby-source-sanity plugin

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -25,6 +27,17 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // Connect to Sanity.io
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: process.env.SANITY_DATASET,
+        watchMode: true,
+        overlayDrafts: true,
+        token: process.env.SANITY_READ_TOKEN
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
