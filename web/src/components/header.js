4de10edb49cade98e2d2ac3f04f9ac9600f567ac
Steven Bister <stevebister@gmail.com>
2020-05-31 10:08:26 +0100

    Added the nav component
    Included the query to the settings so we have a menu

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -2,14 +2,17 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 import Logo from '../images/cakes-with-grace.svg'
+import Nav from './Nav'
 
 const Header = ({siteTitle}) =>  (
-    <header>
-      <Link to='/'>
-        <img src={Logo} alt={siteTitle} />
-      </Link>
-    </header>
-  )
+  <header>
+    <Link to='/'>
+      <img src={Logo} alt={siteTitle} />
+    </Link>
+
+    <Nav />
+  </header>
+)
 
 
 Header.propTypes = {
