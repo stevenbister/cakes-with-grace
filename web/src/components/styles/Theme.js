46537d2e15fbb23658271d097ccf95c33db77119
Steven Bister <stevenbister@macbook-pro.home>
2020-06-16 20:16:27 +0100

    Added a theme file to store variables

---
+++ web/src/components/styles/Theme.js
@@ -0,0 +1,8 @@
+const theme = {
+  titleFont: 'Lalezar, cursive',
+  bodyFont: 'Source Sans Pro, Helvetica, Roboto, Arial, sans-serif',
+  primaryColor: '#4C4A4F',
+  secondaryColor: '#706E72'
+}
+
+export default theme
