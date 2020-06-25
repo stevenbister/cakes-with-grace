4b306ab5b753f7d8a7405b945c63a26e794c99c7
Steven Bister <stevenbister@macbook-pro.home>
2020-06-25 12:41:00 +0100

    Removed: * fluid width so the hero isn't blurry * gatsby image as it's now included in the hero component * Title and main image as they're now included in the hero component Added: * Styled components and a grid * Hero component

--- web/src/templates/Recipe.js
+++ web/src/templates/Recipe.js
@@ -1,13 +1,14 @@
 import { graphql } from 'gatsby'
-import Img from 'gatsby-image'
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 import Categories from '../components/Categories'
 import PortableText from '../components/PortableText'
 import GraphqlErrorList from '../components/GraphqlErrors'
+import Hero from '../components/Hero'
 
 const query = graphql`
   query($id: String!) {
@@ -32,14 +33,26 @@ const query = graphql`
       }
       mainImage {
         asset {
-          fluid(maxWidth: 700) {
+          fluid {
             ...GatsbySanityImageFluid_withWebp
           }
         }
         alternativeText
       }
       _rawMethod
+      publishedAt(formatString: "MMMM DD, YYYY")
     }
+  }
+`
+
+// ?: Will probably need to put this into it's own file for reuse
+const StyledArticle = styled.article`
+  display: grid;
+  grid-template-columns: 1fr minmax(300px, 10fr) 1fr;
+
+  > *:not(header) {
+    grid-column: 2 / 3;
+    width: 100%;
   }
 `
 
@@ -55,7 +68,7 @@ const minutesToHours = ( mins ) => {
     // Take the remainder hours and multiply them by 60 to get the minutes within the hour
     const minutes = (hours - roundHours) * 60;
     const roundMinutes = Math.floor(minutes);
-  
+
     return `${roundHours} ${isPlural(roundHours, 'hour')}, ${roundMinutes} ${isPlural(roundMinutes, 'minute')}`
   }
   return `${mins} ${isPlural(mins, 'minute')}`
@@ -63,53 +76,59 @@ const minutesToHours = ( mins ) => {
 
 const RecipeTemplate = ({ data, errors }) => {
 
-  const { title, categories, timings, ingredients, mainImage, _rawMethod } = data.sanityRecipe
+  const { title, categories, timings, ingredients, mainImage, publishedAt, _rawMethod } = data.sanityRecipe
 
   return (
     <Layout>
-      {/* Let's check for errors and return the error message */}
-      { errors && <SEO title='GraphQL Error' /> }
-      { errors && <GraphqlErrorList errors={errors} /> }
+      <StyledArticle>
+        {/* Let's check for errors and return the error message */}
+        { errors && <SEO title='GraphQL Error' /> }
+        { errors && <GraphqlErrorList errors={ errors } /> }
 
-      {/* If there are no errors lets return the content as normal */}
-      {/* TODO: Set up SEO fully */}
-      { data.sanityRecipe && <SEO title={ title } /> }
-      { data.sanityRecipe && (
-        <>
-          { title && <h1>{ title }</h1> }
+        {/* If there are no errors lets return the content as normal */}
+        {/* TODO: Set up SEO fully */}
+        { data.sanityRecipe && <SEO title={ title } /> }
+        { data.sanityRecipe && (
+          <>
+            { title &&
+              <Hero
+                title={ title }
+                img={ mainImage.asset.fluid }
+                alt={ mainImage.alternativeText }
+                published={ publishedAt } />
+              }
 
-          { mainImage && <Img fluid={ mainImage.asset.fluid } alt={ mainImage.alternativeText } /> }
+            { categories && <Categories categories={ categories } />}
 
-          { categories && <Categories categories={ categories } /> }
+            { timings && (
+              <>
+                <h2>Timings</h2>
 
-          { timings && (
-            <>
-              <h2>Timings</h2>
+                <h3>Prep</h3>
+                <p>{ minutesToHours(timings.prep) }</p>
 
-              <h3>Prep</h3>
-              <p>{ minutesToHours(timings.prep) }</p>
+                <h3>Cook</h3>
+                <p>{ minutesToHours(timings.cook) }</p>
+              </>
+            ) }
 
-              <h3>Cook</h3>
-              <p>{ minutesToHours(timings.cook) }</p>
-            </>
-          ) }
+            { ingredients && (
+              <>
+                <h2>Ingredients</h2>
+                <ul>
+                  {ingredients.map(ingredient => (
+                    <li key={ ingredient._key }>
+                      <span>{ ingredient.amount }</span> <span>{ ingredient.measurement }</span> <span>{ ingredient.name }</span>
+                    </li>
+                  ))}
+                </ul>
+              </>
+            ) }
 
-          { ingredients && (
-            <>
-              <h2>Ingredients</h2>
-              <ul>
-                {ingredients.map(ingredient => (
-                  <li key={ ingredient._key }>
-                    <span>{ ingredient.amount }</span> <span>{ ingredient.measurement }</span> <span>{ ingredient.name }</span>
-                  </li>
-                ))}
-              </ul>
-            </>
-          ) }
-
-          { _rawMethod && <PortableText blocks={_rawMethod} /> }
-        </>
-      ) }
+            { _rawMethod && <PortableText blocks={_rawMethod} /> }
+          </>
+        ) }
+      </StyledArticle>
     </Layout>
   )
 }
