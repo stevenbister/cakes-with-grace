cf75b3c2d811cd5796621d0af91a1d5f6e98cc21
Steven Bister <stevenbister@macbook-pro.home>
2020-06-29 20:58:49 +0100

    Moved styled component into it's own file as it's being reused across templates

---
+++ web/src/components/styles/PageGrid.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const StyledArticle = styled.article`
+    display: grid;
+    grid-template-columns: 1fr minmax(300px, 7fr) 1fr;
+    margin-bottom: 3rem;
+
+    @media (min-width: 768px) {
+      margin-bottom: 6rem;
+    }
+
+    > *:not(header) {
+      grid-column: 2 / 3;
+      width: 100%;
+    }
+`
+
+export default StyledArticle;
