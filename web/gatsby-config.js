32964287bf71bea6213df548207226f899b32bb8
Steven Bister <stevenbister@macbook-pro.home>
2020-07-01 20:24:15 +0100

    Added favicon

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -12,13 +12,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Cakes with Grace`,
+        short_name: `Cakes`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/Favicon.png`, // This path is relative to the root of the site.
       },
     },
     // Google fonts
