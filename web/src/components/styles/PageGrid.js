66ada7696e6bebf4939a138c8af913b3b86e6774
Steven Bister <stevenbister@macbook-pro.home>
2020-07-05 14:19:51 +0100

    Reduced max width so the main content is easier to read

--- web/src/components/styles/PageGrid.js
+++ web/src/components/styles/PageGrid.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const StyledArticle = styled.article`
     display: grid;
-    grid-template-columns: 1fr minmax(300px, 7fr) 1fr;
+    grid-template-columns: 1fr minmax(300px, 3fr) 1fr;
     margin-bottom: 3rem;
 
     @media (min-width: 768px) {
