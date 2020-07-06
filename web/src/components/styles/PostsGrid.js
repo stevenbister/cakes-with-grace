65212c3d005811833392d92290ab28795fabebdd
Steven Bister <stevenbister@macbook-pro.home>
2020-07-06 21:09:02 +0100

    Moved into it's own component
    This will be reused a couple of times so keeping things dry

---
+++ web/src/components/styles/PostsGrid.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const StyledList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 3rem 2rem;
+`
+
+export default StyledList
