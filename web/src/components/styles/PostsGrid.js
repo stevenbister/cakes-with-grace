f18d58b79060c09a1423a2dd23eb6b145e08bd42
Steven Bister <stevenbister@macbook-pro.home>
2020-07-08 20:20:04 +0100

    Added padding to fix positioning
    Adding padding stops the grid sitting flush up agains the hero component

--- web/src/components/styles/PostsGrid.js
+++ web/src/components/styles/PostsGrid.js
@@ -4,6 +4,8 @@ const StyledList = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   grid-gap: 3rem 2rem;
+  /* TODO: set this as a global var */
+  padding-top: calc(3.1875rem + 1.5rem);
 `
 
 export default StyledList
