d0b9fa6265217b11388aa60e7d164d4cf9d00b24
Steven Bister <stevenbister@macbook-pro.home>
2020-07-17 14:29:52 +0100

    Removed unused css
    Decided against keeping the container classes

--- web/src/components/styles/Theme.js
+++ web/src/components/styles/Theme.js
@@ -4,9 +4,6 @@ const theme = {
   primaryColor: '#4C4A4F',
   secondaryColor: '#706E72',
   textShadow: '#3c3c3c 1px 1px 8px',
-  // ? Do I need these?
-  container: '1200px',
-  constinerFluid: '100%',
 }
 
 export default theme
