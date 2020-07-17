38ac07ad55c370829af27f70b247d5046e15f1eb
Steven Bister <stevenbister@macbook-pro.home>
2020-07-17 14:41:11 +0100

    Added grid top padding as a global variable

--- web/src/components/styles/PostsGrid.js
+++ web/src/components/styles/PostsGrid.js
@@ -4,8 +4,7 @@ const StyledList = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   grid-gap: 3rem 2rem;
-  /* TODO: set this as a global var */
-  padding-top: calc(3.1875rem + 1.5rem);
+  padding-top: ${ props => props.theme.gridPaddingTop };
 `
 
 export default StyledList
--- web/src/components/styles/Theme.js
+++ web/src/components/styles/Theme.js
@@ -4,6 +4,7 @@ const theme = {
   primaryColor: '#4C4A4F',
   secondaryColor: '#706E72',
   textShadow: '#3c3c3c 1px 1px 8px',
+  gridPaddingTop: 'calc(3.1875rem + 1.5rem)',
 }
 
 export default theme
