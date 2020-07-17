80da165757834aee606f66011aa5b7a550c251be
Steven Bister <stevenbister@macbook-pro.home>
2020-07-17 14:40:35 +0100

    Added dev details to config
    Not pulling into the portable text component correctly. This fixes that bug

--- web/client-config.js
+++ web/client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
-    projectId: process.env.SANITY_PROJECT_ID,
-    dataset: process.env.SANITY_DATASET,
+    projectId: process.env.SANITY_PROJECT_ID || 'ge4ag6bt',
+    dataset: process.env.SANITY_DATASET || 'dev',
   }
 }
